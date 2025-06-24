ff8710f20a8a34e74718fc01d69af9d3613f412e
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-06-24 08:27:58 +0200

    Remove border from status style

--- views/systeminfo/view.go
+++ views/systeminfo/view.go
@@ -3,8 +3,10 @@ package systeminfoview
 import "swarmcli/styles"
 
 const Height = 6
+const Width = 30
 
 func (m Model) View() string {
 	return styles.StatusStyle.Height(Height).
+		Width(Width).
 		Render(m.content)
 }
