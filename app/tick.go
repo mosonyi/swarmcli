329916eff42e29de3d26f171ca30f0701d22a9ee
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-06-24 10:10:33 +0200

    extract stuff from main into app

---
+++ app/tick.go
@@ -0,0 +1,19 @@
+package app
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	systeminfoview "swarmcli/views/systeminfo"
+	"time"
+)
+
+type tickMsg time.Time
+
+func tick() tea.Cmd {
+	return tea.Tick(time.Second*5, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
+}
+
+func (m Model) handleTick(msg tickMsg) (tea.Model, tea.Cmd) {
+	return m, systeminfoview.LoadStatus()
+}
