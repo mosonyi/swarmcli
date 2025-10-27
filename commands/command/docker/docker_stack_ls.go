dd3049c64832e382b2b14667e247158855248780
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-10-27 12:09:12 +0800

    fix lints

--- commands/command/docker/docker_stack_ls.go
+++ commands/command/docker/docker_stack_ls.go
@@ -2,6 +2,7 @@ package docker
 
 import (
 	"swarmcli/commands/api"
+	stacksview "swarmcli/views/stacks"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -13,6 +14,11 @@ func (DockerStackLs) Name() string        { return "docker stack ls" }
 func (DockerStackLs) Description() string { return "List all Docker stacks" }
 
 func (DockerStackLs) Execute(ctx api.Context, args []string) tea.Cmd {
-	app := ctx.App.(view.Navigator)
-	return app.NavigateTo("stacks", nil)
+	return func() tea.Msg {
+		// Todo: implement stacks view to get stacks from all nodes
+		return view.NavigateToMsg{
+			ViewName: stacksview.ViewName,
+			Payload:  nil,
+		}
+	}
 }
