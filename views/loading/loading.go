d4ae68d4ff601b59e971d3bf7db059d9495984b9
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-10-31 14:44:21 +0800

    add frame to loading screen

--- views/loading/loading.go
+++ views/loading/loading.go
@@ -1,26 +1,38 @@
 package loadingview
 
 import (
+	"fmt"
+	"strings"
+	"swarmcli/views/helpbar"
+
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"swarmcli/views/helpbar"
+
+	"swarmcli/ui"
 	"swarmcli/views/view"
 )
 
 const ViewName = "loading"
 
 type Model struct {
-	Width, Height int
+	width, height int
 	message       string
-	style         lipgloss.Style
+	spinner       spinner.Model
+	visible       bool
 }
 
 func New(width, height int, message string) Model {
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(ui.FrameBorderColor)
+
 	return Model{
-		Width:   width,
-		Height:  height,
+		width:   width,
+		height:  height,
 		message: message,
-		style:   lipgloss.NewStyle().Bold(true).Align(lipgloss.Center).Foreground(lipgloss.Color("205")),
+		spinner: s,
+		visible: true,
 	}
 }
 
@@ -28,12 +40,48 @@ func (m Model) Name() string {
 	return ViewName
 }
 
+func (m Model) Visible() bool {
+	return m.visible
+}
+
+func (m *Model) SetVisible(v bool) {
+	m.visible = v
+}
+
 func (m Model) Init() tea.Cmd {
-	return nil
+	return m.spinner.Tick
 }
 
 func (m Model) Update(msg tea.Msg) (view.View, tea.Cmd) {
-	return m, nil
+	var cmd tea.Cmd
+	m.spinner, cmd = m.spinner.Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	frameStyle := lipgloss.NewStyle().
+		Border(lipgloss.NormalBorder()).
+		Padding(1, 2).
+		Align(lipgloss.Center)
+
+	content := fmt.Sprintf("%s %s", m.spinner.View(), m.message)
+	content = strings.TrimSpace(content)
+
+	frame := frameStyle.Render(content)
+
+	//// Center the frame
+	//xPad := max(0, (m.width-lipgloss.Width(frame))/2)
+	//yPad := max(0, (m.height-lipgloss.Height(frame))/2)
+
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		frame,
+		lipgloss.WithWhitespaceChars(" "),
+		lipgloss.WithWhitespaceForeground(lipgloss.Color("236")),
+	)
 }
 
 func (m Model) ShortHelpItems() []helpbar.HelpEntry {
@@ -42,7 +90,9 @@ func (m Model) ShortHelpItems() []helpbar.HelpEntry {
 	}
 }
 
-func (m Model) View() string {
-	text := m.style.Render(m.message)
-	return lipgloss.Place(m.Width, m.Height, lipgloss.Center, lipgloss.Center, text)
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
