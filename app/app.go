0e4c6d9b912d7f0e821eb427bf08deedc38a8bf3
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-10-31 13:05:20 +0800

    better naming

--- app/app.go
+++ app/app.go
@@ -7,7 +7,7 @@ import (
 	inspectview "swarmcli/views/inspect"
 	logsview "swarmcli/views/logs"
 	nodesview "swarmcli/views/nodes"
-	stackservicesview "swarmcli/views/nodeservices"
+	nodeservicesview "swarmcli/views/nodeservices"
 	stacksview "swarmcli/views/stacks"
 	"swarmcli/views/view"
 
@@ -63,12 +63,12 @@ func Init() {
 		return stacksview.New(w, h), stacksview.LoadStacks(nodeID)
 	})
 
-	registerView(stackservicesview.ViewName, func(w, h int, payload any) (view.View, tea.Cmd) {
+	registerView(nodeservicesview.ViewName, func(w, h int, payload any) (view.View, tea.Cmd) {
 		data, _ := payload.(map[string]interface{})
 		nodeID, _ := data["nodeID"].(string)
 		hostname, _ := data["hostname"].(string)
 
-		v := stackservicesview.New(w, h)
-		return v, stackservicesview.LoadStackServices(nodeID, hostname)
+		v := nodeservicesview.New(w, h)
+		return v, nodeservicesview.LoadStackServices(nodeID, hostname)
 	})
 }
