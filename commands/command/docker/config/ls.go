bf97e8af63c81226785492fce1f3416186b4f664
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-11-09 09:46:11 +0800

    add docker config ls command

---
+++ commands/command/docker/config/ls.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"swarmcli/args"
+	"swarmcli/registry"
+	configsview "swarmcli/views/configs"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+import "swarmcli/views/view"
+
+type DockerConfigLs struct{}
+
+func (DockerConfigLs) Name() string        { return "docker config ls" }
+func (DockerConfigLs) Description() string { return "List service configs" }
+
+func (DockerConfigLs) Execute(ctx any, args args.Args) tea.Cmd {
+	return func() tea.Msg {
+		return view.NavigateToMsg{
+			ViewName: configsview.ViewName,
+			Payload:  nil,
+		}
+	}
+}
+
+var lsCmd = DockerConfigLs{}
+
+func init() {
+	registry.Register(lsCmd)
+}
