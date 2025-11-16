1af101d99c5bf490c82c303bb09f9495ccf79b69
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-11-16 20:42:25 +0800

    remove unnecessary docker node inspect command

--- commands/autoload.go
+++ commands/autoload.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	_ "swarmcli/commands/command"
+	_ "swarmcli/commands/command/docker"
 	_ "swarmcli/commands/command/docker/config"
 	_ "swarmcli/commands/command/docker/node"
 )
