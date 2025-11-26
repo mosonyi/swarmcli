d25a48ed5a7547f69d0ef267790e2697f11bceaa
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-11-26 14:43:24 +0800

    remove unused functions

--- views/configs/view.go
+++ views/configs/view.go
@@ -2,12 +2,9 @@ package configsview
 
 import (
 	"fmt"
-	"io"
 	"swarmcli/ui"
 	filterlist "swarmcli/ui/components/filterable/list"
 
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/docker/docker/api/types/swarm"
 )
@@ -20,23 +17,6 @@ type configItem struct {
 func (i configItem) FilterValue() string { return i.Name }
 func (i configItem) Title() string       { return i.Name }
 func (i configItem) Description() string { return fmt.Sprintf("ID: %s", i.ID) }
-
-type itemDelegate struct{}
-
-func (d itemDelegate) Height() int  { return 1 }
-func (d itemDelegate) Spacing() int { return 0 }
-
-func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
-
-func (d itemDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
-	cfg := item.(configItem)
-	selected := index == m.Index()
-	if selected {
-		fmt.Fprintf(w, "> %s", cfg.Name)
-	} else {
-		fmt.Fprintf(w, "  %s", cfg.Name)
-	}
-}
 
 func configItemFromSwarm(c swarm.Config) configItem {
 	return configItem{Name: c.Spec.Name, ID: c.ID}
