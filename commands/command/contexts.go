6bb396da3aeaadc54d6749f5bc84be4ab03eff1f
Zoltan Mosonyi <mosonyi@esix.hu>
2025-12-07 00:00:47 +0100

    Move alias to its own library

--- commands/command/contexts.go
+++ commands/command/contexts.go
@@ -31,15 +31,3 @@ func init() {
 	registry.Register(aliasCommand{name: "context", target: contextsCmd})
 	registry.Register(aliasCommand{name: "ctx", target: contextsCmd})
 }
-
-// aliasCommand is a simple wrapper to provide aliases for commands
-type aliasCommand struct {
-	name   string
-	target registry.Command
-}
-
-func (a aliasCommand) Name() string        { return a.name }
-func (a aliasCommand) Description() string { return a.target.Description() }
-func (a aliasCommand) Execute(ctx any, args args.Args) tea.Cmd {
-	return a.target.Execute(ctx, args)
-}
