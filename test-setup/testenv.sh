e0af30f7f8f64a492d0ac6556fe8fdbe3d1ffb09
Christian Langenbacher <clangenb+gh@protonmail.ch>
2026-01-26 15:38:26 +0100

    add SPDX headers.

--- test-setup/testenv.sh
+++ test-setup/testenv.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright © 2026 Eldara Tech
+
 set -euo pipefail
 
 # === Config ================================================================
